bf10b2953ff72f9d22370f05af30355cf8a7f877
Fred <59137832+Partchworkboy@users.noreply.github.com>
2023-04-27 20:44:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import Calendar from 'react-calendar';
+import './App.css'
 import 'react-datepicker/dist/react-datepicker.css';
 
 const EventForm = ({ addEvent }) => {
