dd807c379c814914a6e5f2704e0cef38bc656dd3
Fred <59137832+Partchworkboy@users.noreply.github.com>
2023-04-27 22:38:46 +0300

    Create Events.js

---
+++ src/Events.js
@@ -0,0 +1,12 @@
+// events.js
+
+const eventsData = [
+  { 
+    eventName: 'Placeholder Event',
+    startDate: new Date(),
+    endDate: new Date(),
+    done: false
+  }
+];
+
+export default eventsData;
